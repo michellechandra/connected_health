05e9a569cd705c3ddd186e3ab7a1b589bb21c01a
michellechandra <mcc448@nyu.edu>
2015-05-05 23:49:04 -0400

    adding new code

--- SerialToBrowser 2/public/main.js
+++ SerialToBrowser 2/public/main.js
@@ -1,21 +1,40 @@
 window.onload = function onLoad() {
     
-    var circle = new ProgressBar.Circle('#progress', {
+    circle = new ProgressBar.Circle('#progress', {
         color: '#674172',
-        duration: 3000,
+        duration: 300,
    	    strokeWidth: 3,
         trailWidth: 1,
         easing: 'easeInOut'
     });
 
-    var circleIndoor = new ProgressBar.Circle('#progressIndoor', {
+    circleIndoor = new ProgressBar.Circle('#progressIndoor', {
         color: '#1BA29B',
-        duration: 3000,
+        duration: 300,
    	    strokeWidth: 3,
         trailWidth: 1,
         easing: 'easeInOut'
     });
 
-    circle.animate(1);
-    circleIndoor.animate(1);
+    //circle.animate(1, function() {
+        //circle.setText(second);
+    //});
+    //circleIndoor.animate(1);
 };
+
+var circle;
+var circleIndoor;
+
+function updateIndoorRating(number) {
+    circle.animate(number, function() {
+        //console.log('here');
+        document.getElementById('thingy').innerHTML = number;
+    });
+}
+
+function updateOutdoorRating(number) {
+circleIndoor.animate(number, function(){
+        //circleIndoor.setText(number);
+        document.getElementById('thing').innerHTML = number;
+    });
+}
